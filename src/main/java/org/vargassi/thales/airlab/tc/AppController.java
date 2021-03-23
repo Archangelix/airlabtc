442b81c6b23ef9fc6f1d640463603c8fb978ae28
Archangelix <iwanstr@gmail.com>
2021-03-23 22:03:02 +0800

    Initial commit

---
+++ src/main/java/org/vargassi/thales/airlab/tc/AppController.java
@@ -0,0 +1,14 @@
+package org.vargassi.thales.airlab.tc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
