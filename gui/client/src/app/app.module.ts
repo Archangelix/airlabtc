ce8d057ed90de1bad710569e8689c20bcf4dcfa1
Iwan Satria <angelo_vargassi@yahoo.com>
2021-03-27 22:50:29 +0800

    Removing unused components and services.

--- gui/client/src/app/app.module.ts
+++ gui/client/src/app/app.module.ts
@@ -5,17 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AirportListComponent } from './airport-list/airport-list.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserFormComponent } from './user-form/user-form.component';
 import { AirportService } from './service/airport.service';
-import { UserService } from './service/user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AirportListComponent,
-    UserListComponent,
-    UserFormComponent
+    AirportListComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +18,7 @@ import { UserService } from './service/user.service';
     HttpClientModule,
     FormsModule
   ],
-  providers: [AirportService, UserService],
+  providers: [AirportService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
